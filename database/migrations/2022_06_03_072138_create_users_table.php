f3542b9c2c248b4983fb8d0ece4cf2b4dfe0c4b7
numaanjaved <naumaan.javed@hotmail.com>
2022-06-03 16:47:41 +0500

    initial commit

---
+++ database/migrations/2022_06_03_072138_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 50);
+            $table->string('password', 255);
+            $table->boolean('status');
+
+            $table->timestamps();
+            $table->bigInteger('updated_by');
+            $table->softDeletes('deleted_at', 0);
+            $table->bigInteger('deleted_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
